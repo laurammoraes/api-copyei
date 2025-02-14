ebca3fa6658fbb2e7c6c028c66f654eb93321c3b
laurammoraes2@gmail.com <laura@poontoagencia.com>
2025-02-13 21:24:31 -0300

    Add ajustes de codigo

--- src/services/uploadWebsiteToDrive.js
+++ src/services/uploadWebsiteToDrive.js
@@ -10,12 +10,11 @@ import { removeWatermark } from "../utils/removeWatermark.js";
 import { removeEditabilityFromSite } from "../utils/removeEditabilityFromSite.js";
 import { updateLoadingState } from "./websocket.js";
 
-/* Função para aguardar um intervalo de tempo */
+
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-/* Função para criar arquivo no Drive e torná-lo público */
 async function createPublicFile(drive, fileMetadata, media) {
   try {
     const fileResponse = await drive.files.create({
@@ -24,10 +23,10 @@ async function createPublicFile(drive, fileMetadata, media) {
       fields: "id, webViewLink, webContentLink",
     });
 
-    /* Aguarda 1 segundo antes de aplicar permissões para evitar erros */
+    
     await sleep(1000);
 
-    /* Tornar o arquivo público */
+
     await drive.permissions.create({
       fileId: fileResponse.data.id,
       requestBody: {
@@ -43,22 +42,22 @@ async function createPublicFile(drive, fileMetadata, media) {
 }
 
 async function uploadFolderToDrive(drive, localPath, driveParentId) {
-  /* Ler conteúdo do diretório */
+
   const entries = await fs.readdir(localPath, { withFileTypes: true });
 
   for (const entry of entries) {
     const entryPath = path.join(localPath, entry.name);
 
-    /* Fazer distinção entre arquivo e pasta */
+
     if (entry.isDirectory()) {
-      /* Se for uma pasta, criar sua equivalência no Google Drive */
+
       const folderMetadata = {
         name: entry.name,
         mimeType: "application/vnd.google-apps.folder",
         parents: [driveParentId],
       };
 
-      /* Criar pasta no Google Drive */
+
       const folderResponse = await drive.files.create({
         requestBody: folderMetadata,
         fields: "id",
@@ -66,10 +65,10 @@ async function uploadFolderToDrive(drive, localPath, driveParentId) {
 
       const folderId = folderResponse.data.id;
 
-      /* Recursividade para enviar os arquivos dos subdiretórios */
+      
       await uploadFolderToDrive(drive, entryPath, folderId);
     } else if (entry.isFile()) {
-      /* Definir metadados do arquivo */
+    
       const fileMetadata = {
         name: entry.name,
         parents: [driveParentId],
@@ -80,44 +79,30 @@ async function uploadFolderToDrive(drive, localPath, driveParentId) {
       };
 
       await createPublicFile(drive, fileMetadata, media);
-      // /* Criar arquivo no Google Drive */
-      // const fileResponse = await drive.files.create({
-      //   requestBody: fileMetadata,
-      //   media: media,
-      //   fields: "id, webViewLink, webContentLink",
-      // });
-
-      // /* Tornar o arquivo público */
-      // await drive.permissions.create({
-      //   fileId: fileResponse.data.id,
-      //   requestBody: {
-      //     role: "reader",
-      //     type: "anyone",
-      //   },
-      // });
+      
     }
   }
 }
 
 export async function uploadWebsiteToDrive(websiteDomain, decodedJWT) {
-  /* Definir o caminho do arquivo para fazer upload */
+  
   const websiteDirectory = path.join(
     process.env.COPYEI_WEBSITES_OUTPUT_DIRECTORY,
     websiteDomain
   );
 
-  /* Verificar se o diretório existe */
+  
   if (!existsSync(websiteDirectory)) {
     throw new Error("Arquivo local não encontrado");
   }
 
-  /* Remover marca da água */
+  
   await removeWatermark(websiteDirectory);
 
-  /* Remover editor, caso presente */
+  
   await removeEditabilityFromSite(websiteDirectory);
 
-  /* Obter credenciais do Google Drive */
+
   oauth2Client.setCredentials(decodedJWT);
   const drive = google.drive({ version: "v3", auth: oauth2Client });
 
@@ -127,7 +112,7 @@ export async function uploadWebsiteToDrive(websiteDomain, decodedJWT) {
       mimeType: "application/vnd.google-apps.folder",
     };
 
-    /* Criar uma pasta dentro go google drive */
+
     const folderResponse = await drive.files.create({
       requestBody: folderMetadata,
       fields: "id",
@@ -135,7 +120,7 @@ export async function uploadWebsiteToDrive(websiteDomain, decodedJWT) {
 
     const rootFolderId = folderResponse.data.id;
 
-    /* Tornar a pasta pública */
+
     await drive.permissions.create({
       fileId: rootFolderId,
       requestBody: {
@@ -146,7 +131,7 @@ export async function uploadWebsiteToDrive(websiteDomain, decodedJWT) {
 
     await uploadFolderToDrive(drive, websiteDirectory, rootFolderId);
 
-    /* Atualizar site */
+
     await prisma.websites.update({
       where: {
         title: websiteDomain,
@@ -157,10 +142,10 @@ export async function uploadWebsiteToDrive(websiteDomain, decodedJWT) {
       },
     });
 
-    /* Delete pasta local */
+
     await fs.rm(websiteDirectory, { recursive: true });
 
-    /* Atualizar estado do websocket */
+
     updateLoadingState(websiteDomain);
 
     console.log(`Upload de ${websiteDomain} concluído com sucesso.`);
